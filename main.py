b4a03e486f85f17d1a93bfd109db636ff00b7d12
BIOWP <bioweapon@126.com>
2018-06-06 00:20:00 +0800

    Major Changes

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
-from urllib import request
-from urllib import parse
+from urllib import request, parse
 from html.parser import HTMLParser
 
-import os, time, json, threading, queue
+import os, time, json, threading, queue, re
 
 
-# list of jobs
-JOB_LIST_FILENAME = 'targets.txt'
+# Basics
+NumThreads = 16
+MaxNumOfFiles = 20000
+PageLimit = 1000
+BaseUrl = "https://archives.bulbagarden.net/w/index.php"
+QueryModel = {
+    "title": "Special:MIMESearch",
+    "mime": "image/png",
+    "limit": PageLimit,
+    "offset": 0
+}
 
-# the grand folder to contain all images to download
-TARGET_FOLDER_NAME = 'Sprites_all'
+# The folder to contain all images to download
+GrandFolder = "Sprites_all"
+
+# The file to record queued tasks
+JobsQueuedFilename = 'queue.txt'
+# The file to record done tasks
+JobsDoneFilename = 'done.txt'
+
+# Only files matched these patterns will be downloaded
+# File matched the i-th pattern will be downloaded to a folder named "Pattern[i]"
+Patterns = [
+    "[0-9]{3}[A-Z]*[A-z]*MS\.png",
+    "120px-Shuffle[0-9]{3}\.png",
+    "120px-[0-9]{3}[A-Z|a-z]{1}[A-Z|a-z|\.|\-|\:]*\.png"
+]
 
 # in seconds
-SYNC_INTERVAL = 15
+SyncIntervalS = 15
 
-USER_AGENT = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-NUM_THREADS = 16
-
-
-if not os.path.isdir(TARGET_FOLDER_NAME):
-    os.mkdir(TARGET_FOLDER_NAME)
+DefaultUserAgent = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
 
 
+
+if not os.path.isdir(GrandFolder):
+    os.mkdir(GrandFolder)
+if not os.path.isfile(JobsQueuedFilename):
+    with open(JobsQueuedFilename, 'w') as F:
+        F.write('[]')
+if not os.path.isfile(JobsDoneFilename):
+    with open(JobsDoneFilename, 'w') as F:
+        F.write('[]')
 
 def get_raw_data(url, headers=dict()):
-        headers['User-Agent'] = USER_AGENT
+        headers['User-Agent'] = DefaultUserAgent
         try:
             req = request.Request(url, headers=headers)
             resp = request.urlopen(req)
@@ -40,17 +65,17 @@ def get_raw_data(url, headers=dict()):
 
 
 
-def download_file(url, filename='', headers=dict()):
-        filename = filename or url.split('/')[-1]
-        headers['User-Agent'] = USER_AGENT
+def download_file(src, dst='', headers=dict()):
+        dst = dst or url.split('/')[-1]
+        headers['User-Agent'] = DefaultUserAgent
         try:
-            req = request.Request(url, headers=headers)
+            req = request.Request(src, headers=headers)
             resp = request.urlopen(req)
-            with open(filename, 'wb') as F:
+            with open(dst, 'wb') as F:
                 F.write(resp.read())
             return True
         except Exception as e:
-            printQueue.put("Error while downloading " + url + ":" + str(e))
+            printQueue.put("Error while downloading " + src + ":" + str(e))
             return False
 
 
@@ -58,15 +83,20 @@ def download_file(url, filename='', headers=dict()):
 class MyHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
-        self.base_url = 'https://archives.bulbagarden.net/'
+        self.base_url = BaseUrl
         self.img_urls = set()
     
     def handle_starttag(self, tag, attrs):
-        if tag == 'img':
+        if tag == 'a':
+            url = ''
+            isTarget = False
             for k, v in attrs:
-                if k == 'src':
+                if k == 'href':
                     url = parse.urljoin(self.base_url, v)
-                    self.img_urls.add(url)
+                elif k == 'class' and v == 'internal':
+                    isTarget = True
+            if isTarget and url:
+                self.img_urls.add(url)
 
 
 def worker_print():
@@ -79,12 +109,14 @@ def worker_print():
 def worker_sync():
     lastSyncTime = time.time()
     while True:
-        syncRequestQueue.get()
+        forced = syncRequestQueue.get()
         currentTime = time.time()
-        if currentTime - lastSyncTime >= SYNC_INTERVAL:
-            printQueue.put("Syncing to " + JOB_LIST_FILENAME)
-            with open(JOB_LIST_FILENAME, 'w') as f:
+        if forced or currentTime - lastSyncTime >= SyncIntervalS:
+            printQueue.put("Syncing Progess...")
+            with open(JobsQueuedFilename, 'w') as f:
                 f.write(json.dumps(list(jobQueue.queue)))
+            with open(JobsDoneFilename, 'w') as f:
+                f.write(json.dumps(jobsDoneList))
             lastSyncTime = currentTime
         syncRequestQueue.task_done()
 
@@ -93,54 +125,86 @@ def worker():
     while True:
         job = jobQueue.get()
         
-        if job[0] == 'parse': # new Pokemon page
-            dex, name = job[1], job[2].split(':')[-1]
-            printQueue.put("Parsing Pokemon Page: " + name)
+        if job.get('cmd') == 'parse': # new Pokemon page
+            pageUrl = job.get('url')
             
-            data = get_raw_data(job[2])
+            printQueue.put("Parsing Page: " + pageUrl)
+            data = get_raw_data(pageUrl)
+            
             p = MyHTMLParser()
             p.feed(data)
             for url in p.img_urls:
                 filename = url.split('/')[-1]
-                if dex in filename or name.lower() in filename.lower():
-                    subFolderPath = os.path.join(TARGET_FOLDER_NAME, dex)
-                    filepath = os.path.join(subFolderPath, filename)
-                    if not os.path.isdir(subFolderPath):
-                        os.mkdir(subFolderPath)
-                    if not os.path.isfile(filepath):
-                        jobQueue.put(['retrieve', url, filepath])
+                for i, pattern in enumerate(Patterns):
+                    if re.fullmatch(pattern, filename):
+                        subFolderPath = os.path.join(GrandFolder, "Pattern_" + str(i+1))
+                        destinationPath = os.path.join(subFolderPath, filename)
+                        jobQueue.put({
+                            "cmd": "retrieve",
+                            "url": url,
+                            "dst": destinationPath,
+                            "dstdir": subFolderPath
+                        })
+                        break
                         
-        elif job[0] == 'retrieve': # download an image
-            printQueue.put("Downloading: " + job[1])
-            download_file(job[1], job[2])
+        elif job.get('cmd') == 'retrieve': # download an image
+            url, dstdir, dst = job.get('url'), job.get('dstdir'), job.get('dst')
+            if not os.path.isdir(dstdir):
+                os.mkdir(dstdir)
+            if not os.path.isfile(dst):
+                printQueue.put("Downloading: " + url)
+                download_file(url, dst)
 
-        syncRequestQueue.put(0)
+        if jobQueue.empty(): # All jobs done, forced sync
+            syncRequestQueue.put(1)
+        else:
+            syncRequestQueue.put(0)
+            
+        jobsDoneList.append(job)
         jobQueue.task_done()
 
 
-
-
-
 printQueue = queue.Queue()
-pt = threading.Thread(target=worker_print)
-pt.daemon = True
-pt.start()
-
 syncRequestQueue = queue.Queue()
-st = threading.Thread(target=worker_sync)
-st.daemon = True
-st.start()
-
-
 jobQueue = queue.Queue()
-with open(JOB_LIST_FILENAME) as f:
-    for j in json.load(f):
-        jobQueue.put(j)
+jobsDoneList = []
 
-for i in range(NUM_THREADS):
-     t = threading.Thread(target=worker)
-     t.daemon = True
-     t.start()
+def main():
+    pt = threading.Thread(target=worker_print)
+    pt.daemon = True
+    pt.start()
+    
+    st = threading.Thread(target=worker_sync)
+    st.daemon = True
+    st.start()
 
-jobQueue.join()
-os.system('shutdown /t 60')
+    startOffset = 0
+    with open(JobsDoneFilename) as f:
+        for j in json.load(f):
+            jobsDoneList.append(j)
+            if j.get('cmd') == 'parse':
+                q = parse.parse_qs(parse.urlparse(j.get('url')).query)
+                startOffset = max(int(q.get('offset', [0])[0]), startOffset) 
+    with open(JobsQueuedFilename) as f:
+        for j in json.load(f):
+            jobQueue.put(j)
+            if j.get('cmd') == 'parse':
+                q = parse.parse_qs(parse.urlparse(j.get('url')).query)
+                startOffset = max(int(q.get('offset', [0])[0]), startOffset)
+                
+    offset = startOffset
+    while offset < MaxNumOfFiles:
+        q = QueryModel.copy()
+        q['offset'] = offset
+        jobQueue.put({
+            "cmd": "parse",
+            "url": BaseUrl + '?' + parse.urlencode(q)
+        })
+        offset += PageLimit
+            
+    for i in range(NumThreads):
+         t = threading.Thread(target=worker)
+         t.daemon = True
+         t.start()
+
+    jobQueue.join()
