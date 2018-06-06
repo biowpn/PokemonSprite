0eb504ca093527b6fce7847b7777ccf4e498de46
BIOWP <bioweapon@126.com>
2018-06-06 13:17:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from urllib import request, parse
 from html.parser import HTMLParser
 
-import os, time, json, threading, queue, re
+import os, time, json, threading, queue, re, shutil
 
 
 # Basics
@@ -22,12 +22,39 @@ GrandFolder = "Sprites_all"
 # The file to record queued tasks
 JobsQueuedFilename = 'queue.txt'
 
-# Only files matched these patterns will be downloaded
-# File matched the i-th pattern will be downloaded to a folder named "Pattern[i]"
+# How files matched the pattern will be handled in order.
+# Each logic is a dictionary-like object with the following attributes:
+#   - [cmd]
+#   - [pat]
+#   - [dst] (Optional)
+# [cmd] may take the following values:
+#   - "save": the file will be saved to a folder specified by [dst].
+#   - "skip": skip this file. The subsequenet handling logics will be ignored
+
 Patterns = [
-    "[0-9]{3}[A-Z]*[A-z]*MS\.png",
-    "Shuffle[0-9]{3}\.png",
-    "[0-9]{3}[A-Z|a-z]{1}[A-Z|a-z|\.|\-|\:]*\.png"
+    {
+        "cmd": "save",
+        "pat": "[0-9]{3}[A-Z]?[A-Z|a-z]*MS\.png",
+        "dst": "Pattern_1"
+    },
+    {
+        "cmd": "save",
+        "pat": "Shuffle[0-9]{3}\.png",
+        "dst": "Pattern_2"
+    },
+    {
+        "cmd": "skip",
+        "pat": ".*?_Channel\.png"
+    },
+    {
+        "cmd": "skip",
+        "pat": ".*?_Dream_.*?"
+    },
+    {
+        "cmd": "save",
+        "pat": "[0-9]{3}[A-Z][a-z]+([\.|\-|\:|\_][A-Z][a-z]*)*\.png",
+        "dst": "Pattern_3"
+    }
 ]
 
 # in seconds
@@ -43,6 +70,31 @@ if not os.path.isfile(JobsQueuedFilename):
     with open(JobsQueuedFilename, 'w') as F:
         F.write('[]')
 
+# Organizing files
+def iter_files(directory, pattern, cmd = 'count'):
+    count = 0
+    for file in os.listdir(directory):
+        if re.fullmatch(pattern, file):
+            count += 1
+            fullFilepath = os.path.join(directory, file)
+            cmds = cmd.split()
+            if cmds[0] in ['sc', 'scan']:
+                print(fullFilepath)
+            elif cmds[0] == ['cp', 'copy']:
+                if not os.path.isdir(cmds[1]):
+                    os.mkdir(cmds[1])
+                shutil.copyfile(fullFilepath, cmds[1])
+            elif cmds[0] == ['mv', 'move']:
+                if not os.path.isdir(cmds[1]):
+                    os.mkdir(cmds[1])
+                shutil.move(fullFilepath, cmds[1])
+            elif cmds[0] == ['del', 'delete']:
+                os.remove(fullFilepath)
+    if cmds[0] in ['ct', 'count']:
+        return count
+
+
+# Get raw HTML codes from a page specified by [url]
 def get_raw_data(url, headers=dict()):
         headers['User-Agent'] = DefaultUserAgent
         try:
@@ -58,8 +110,7 @@ def get_raw_data(url, headers=dict()):
             printQueue.put("Error while accessing " + url + ":" + str(e))
             return ''
 
-
-
+# Download a file and save it to a directory specified by [dst]
 def download_file(src, dst='', headers=dict()):
         dst = dst or url.split('/')[-1]
         headers['User-Agent'] = DefaultUserAgent
@@ -74,7 +125,7 @@ def download_file(src, dst='', headers=dict()):
             return False
 
 
-# To extract all unique images from a page
+# To extract all unique image URLs from a page
 class MyHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -129,27 +180,28 @@ def worker():
             p.feed(data)
             for url in p.img_urls:
                 filename = url.split('/')[-1]
-                for i, pattern in enumerate(Patterns):
-                    if re.fullmatch(pattern, filename):
-                        subFolderPath = os.path.join(GrandFolder, "Pattern_" + str(i+1))
-                        destinationPath = os.path.join(subFolderPath, filename)
-                        jobQueue.put({
-                            "cmd": "retrieve",
-                            "url": url,
-                            "dst": destinationPath,
-                            "dstdir": subFolderPath
-                        })
-                        break
+                for i, logic in enumerate(Patterns):
+                    if re.fullmatch(logic.get('pat'), filename):
+                        if logic.get('cmd') == 'save':
+                            dst = os.path.join(GrandFolder, logic.get('dst', "Pattern_" + str(i+1)), filename)
+                            jobQueue.put({
+                                "cmd": "retrieve",
+                                "url": url,
+                                "dst": dst
+                            })
+                        elif logic.get('cmd') == 'skip':
+                            break
                         
         elif job.get('cmd') == 'retrieve': # download an image
-            url, dstdir, dst = job.get('url'), job.get('dstdir'), job.get('dst')
+            url, dst = job.get('url'), job.get('dst')
+            dstdir = os.path.split(dst)[0]
             if not os.path.isdir(dstdir):
                 os.mkdir(dstdir)
             if not os.path.isfile(dst):
                 printQueue.put("Downloading: " + url)
                 download_file(url, dst)
 
-        if jobQueue.empty(): # All jobs done, forced sync
+        if jobQueue.empty(): # All jobs done, force sync
             syncRequestQueue.put(1)
         else:
             syncRequestQueue.put(0)
@@ -161,32 +213,32 @@ printQueue = queue.Queue()
 syncRequestQueue = queue.Queue()
 jobQueue = queue.Queue()
 
-def main():
-    pt = threading.Thread(target=worker_print)
-    pt.daemon = True
-    pt.start()
-    
-    st = threading.Thread(target=worker_sync)
-    st.daemon = True
-    st.start()
 
-    with open(JobsQueuedFilename) as f:
-        for j in json.load(f):
-            jobQueue.put(j)
-                
-    offset = 0
-    while offset < MaxNumOfFiles:
-        q = QueryModel.copy()
-        q['offset'] = offset
-        jobQueue.put({
-            "cmd": "parse",
-            "url": BaseUrl + '?' + parse.urlencode(q)
-        })
-        offset += PageLimit
+pt = threading.Thread(target=worker_print)
+pt.daemon = True
+pt.start()
+
+st = threading.Thread(target=worker_sync)
+st.daemon = True
+st.start()
+
+with open(JobsQueuedFilename) as f:
+    for j in json.load(f):
+        jobQueue.put(j)
             
-    for i in range(NumThreads):
-         t = threading.Thread(target=worker)
-         t.daemon = True
-         t.start()
+offset = 0
+while offset < MaxNumOfFiles:
+    q = QueryModel.copy()
+    q['offset'] = offset
+    jobQueue.put({
+        "cmd": "parse",
+        "url": BaseUrl + '?' + parse.urlencode(q)
+    })
+    offset += PageLimit
+        
+for i in range(NumThreads):
+     t = threading.Thread(target=worker)
+     t.daemon = True
+     t.start()
 
-    jobQueue.join()
+jobQueue.join()
